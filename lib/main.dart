35d18fa6fdc1da7f58e1758747b28f7b9300e0cb
Obasan Tolulope <obasantolu@gmail.com>
2024-05-07 09:58:08 +0100

    Added the googlefonts for textStyling

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker_app/screens/home/home.dart';
 import 'package:expense_tracker_app/src/components/persistent_nav.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,8 +14,11 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: Size(375, 812),
       builder: (_, child) {
-        return const MaterialApp(
-            title: 'My Expense tracker', home: PersistentNav());
+        return MaterialApp(
+          title: 'My Expense tracker',
+          home: PersistentNav(),
+          theme: ThemeData(fontFamily: GoogleFonts.montserrat().fontFamily),
+        );
       },
     );
   }
--- lib/src/components/app_bar.dart
+++ lib/src/components/app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyAppBar extends StatelessWidget {
   const MyAppBar({super.key});
@@ -13,17 +14,31 @@ class MyAppBar extends StatelessWidget {
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Good Morning,'),
+            Text(
+              'Good Morning,',
+              style: TextStyle(fontSize: 16.sp),
+            ),
             addHeight(2),
-            Text('Tolulope Obasan' style: TextStyle(fontS: ),),
+            Text(
+              'Tolulope Obasan',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20.sp),
+            ),
           ],
         ),
         Spacer(),
         Row(
           children: [
             IconButton(
-                onPressed: () {}, icon: Icon(Icons.notifications_outlined)),
-            CircleAvatar(child: Icon(Icons.person))
+              onPressed: () {},
+              icon: Icon(Icons.notifications_outlined),
+              iconSize: 30,
+            ),
+            addWidth(5),
+            CircleAvatar(
+              child: Icon(Icons.person),
+              radius: 30,
+              backgroundColor: Colors.orange.shade700,
+            )
           ],
         )
       ],
