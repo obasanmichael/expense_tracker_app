826dcefb9dd711bbd061970ee0923a64fb410e93
Obasan Tolulope <obasantolu@gmail.com>
2024-05-07 13:00:51 +0100

    designed part of the UI of the home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,14 @@
+import 'package:expense_tracker_app/src/components/amount_container.dart';
 import 'package:expense_tracker_app/src/components/app_bar.dart';
-import 'package:expense_tracker_app/src/components/persistent_nav.dart';
+import 'package:expense_tracker_app/src/components/expenses_chart.dart';
+import 'package:expense_tracker_app/src/components/list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  Widget addHeight(double height) => SizedBox(height: height.h);
+  Widget addWidth(double width) => SizedBox(width: width.w);
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +16,42 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(20.0),
             child: Column(
-              children: [MyAppBar()],
+              children: [
+                MyAppBar(),
+                addHeight(25),
+                Row(
+                  children: [
+                    Expanded(child: AmountContainer()),
+                    // Spacer(),
+                    addWidth(15),
+                    Expanded(child: AmountContainer()),
+                  ],
+                ),
+                addHeight(20),
+                ExpensesChart(),
+                addHeight(20),
+                Row(
+                  children: [
+                    Text('Last 10 transactions',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w800, fontSize: 12.sp)),
+                    Spacer(),
+                    Text('view all'.toUpperCase(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.black))
+                  ],
+                ),
+                addHeight(15),
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      MyListTile(),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
