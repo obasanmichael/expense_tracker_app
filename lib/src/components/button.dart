1fdd508f98ccd64fe1084cff2747de26ff6d8677
Obasan Tolulope <obasantolu@gmail.com>
2024-05-08 09:14:52 +0100

    created a button component

---
+++ lib/src/components/button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Button extends StatelessWidget {
+  const Button({
+    super.key,
+    required this.backgroundColor,
+    required this.text,
+  });
+
+  final String text;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    Color defaultColor = Color(0xffE97451);
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            backgroundColor: defaultColor ?? backgroundColor,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.r)),
+            padding: EdgeInsets.symmetric(vertical: 12.h)),
+        child: Text(
+          text,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 15.sp,
+              fontWeight: FontWeight.w600),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
