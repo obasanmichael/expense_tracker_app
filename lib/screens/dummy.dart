15ee5e4c5c1a52978069cac3f46e684a230ff67c
Obasan Tolulope <obasantolu@gmail.com>
2024-05-08 09:14:38 +0100

    working with the ui

--- lib/screens/dummy.dart
+++ lib/screens/dummy.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_app/src/components/button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -15,17 +16,49 @@ class _DummyScreenState extends State<DummyScreen> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 12.w),
+      padding: EdgeInsets.symmetric(vertical: 15.h, horizontal: 15.w),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Add a new Expense'),
+          Text(
+            'Add a new Expense',
+            style: TextStyle(
+                fontWeight: FontWeight.w700, color: Colors.grey.shade600),
+          ),
           addHeight(2),
           TextField(
-            decoration: const InputDecoration(label: Text('Title')),
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade300),
+                  borderRadius: BorderRadius.circular(10.r)),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 15.h, horizontal: 15.w),
+              labelText: 'Title',
+              // hintText: '',
+            ),
           ),
-          addHeight(3),
-          Text('Add an amount'),
-          TextField(decoration: InputDecoration(label: Text('Amount')))
+          addHeight(25),
+          Text(
+            'Add an amount',
+            style: TextStyle(
+                fontWeight: FontWeight.w700, color: Colors.grey.shade600),
+          ),
+          addHeight(5),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade300),
+                  borderRadius: BorderRadius.circular(10.r)),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 15.h, horizontal: 15.w),
+              labelText: 'Amount',
+              hintText: 'Enter your Amount',
+            ),
+          ),
+          addHeight(10),
+          Text('Select a Category of expenses'),
+          Spacer(),
+          Button(backgroundColor: null, text: 'Add expense')
         ],
       ),
     );
