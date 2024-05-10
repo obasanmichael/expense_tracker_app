c29faf0a6f5165f22c5f13c9ca07da48bdf0681e
Obasan Tolulope <obasantolu@gmail.com>
2024-05-10 16:39:05 +0100

    made the category button dynamic

--- lib/src/components/category_button.dart
+++ lib/src/components/category_button.dart
@@ -1,29 +1,41 @@
+import 'package:expense_tracker_app/models/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoryButton extends StatelessWidget {
-  const CategoryButton({super.key});
+  const CategoryButton({super.key, required this.category});
+
+  final Category category;
 
   @override
   Widget build(BuildContext context) {
-    return OutlinedButton(
-      style: OutlinedButton.styleFrom(
-          padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 20.w),
-          backgroundColor: Colors.transparent,
-          side: BorderSide(
-            color: Colors.grey.shade400,
+    return Row(
+      children: [
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 20.w),
+              backgroundColor: Colors.transparent,
+              side: BorderSide(
+                color: Colors.grey.shade400,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.r),
+              ),
+              elevation: 0,
+              foregroundColor: Colors.grey.shade700),
+          onPressed: () {},
+          child: Text(
+            category.toString().split('.').last,
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontSize: 14.sp),
           ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.r),
-          ),
-          elevation: 0,
-          foregroundColor: Colors.grey.shade700),
-      onPressed: () {},
-      child: Text(
-        'food',
-        style: TextStyle(
-            color: Colors.black, fontWeight: FontWeight.w600, fontSize: 14.sp),
-      ),
+        ),
+        SizedBox(
+          width: 10.w,
+        )
+      ],
     );
   }
 }
