55dfc7974dbd77ddd8aca76879eaec18c5938cab
Obasan Tolulope <obasantolu@gmail.com>
2024-05-10 14:02:44 +0100

    Made the bar chart and expense chart component

---
+++ lib/src/components/expenses_chart/expenses_chart.dart
@@ -0,0 +1,22 @@
+import 'package:expense_tracker_app/src/components/expenses_chart/bar_chat.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ExpensesChart extends StatelessWidget {
+  const ExpensesChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250.h,
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(vertical: 15.h, horizontal: 15.h),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.r),
+        color: Colors.black,
+      ),
+      child: MyBarChat(),
+    );
+  }
+}
