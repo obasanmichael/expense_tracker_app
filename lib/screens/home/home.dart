6b92a58f4bc69e55fa8081298b60cfed6b3a7054
Obasan Tolulope <obasantolu@gmail.com>
2024-05-10 14:01:54 +0100

    made the Amount Container dynamic and made little changes to the home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,58 +1,95 @@
+import 'package:expense_tracker_app/models/expense.dart';
 import 'package:expense_tracker_app/src/components/amount_container.dart';
 import 'package:expense_tracker_app/src/components/app_bar.dart';
-import 'package:expense_tracker_app/src/components/expenses_chart.dart';
-import 'package:expense_tracker_app/src/components/list_tile.dart';
+import 'package:expense_tracker_app/src/components/expenses_chart/expenses_chart.dart';
+import 'package:expense_tracker_app/widgets/expense/expense_item.dart';
+import 'package:expense_tracker_app/widgets/expense/expenses_list.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Expense> _registeredExpense = [
+    Expense(
+        title: 'Food',
+        date: DateTime.now(),
+        amount: 12000,
+        category: Category.food),
+    Expense(
+        title: 'Movies',
+        date: DateTime.now(),
+        amount: 10000,
+        category: Category.movies),
+    Expense(
+        title: 'medication',
+        date: DateTime.now(),
+        amount: 12000,
+        category: Category.health)
+  ];
+
   Widget addHeight(double height) => SizedBox(height: height.h);
+
   Widget addWidth(double width) => SizedBox(width: width.w);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                MyAppBar(),
-                addHeight(25),
-                Row(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              MyAppBar(),
+              addHeight(25),
+              Row(
+                children: [
+                  Expanded(
+                      child: AmountContainer(
+                    heading: 'Earning this month',
+                    bgColor: Colors.green.shade100,
+                    price: '20,000',
+                  )),
+                  // Spacer(),
+                  addWidth(15),
+                  Expanded(
+                      child: AmountContainer(
+                    heading: 'Expenses this month',
+                    bgColor: Color(0xffF4C2C2),
+                    price: '10,000',
+                  )),
+                ],
+              ),
+              addHeight(20),
+              ExpensesChart(),
+              addHeight(20),
+              Row(
+                children: [
+                  Text('Last 10 transactions',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800, fontSize: 12.sp)),
+                  Spacer(),
+                  Text('view all'.toUpperCase(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black))
+                ],
+              ),
+              addHeight(15),
+              Expanded(
+                child: Column(
                   children: [
-                    Expanded(child: AmountContainer()),
-                    // Spacer(),
-                    addWidth(15),
-                    Expanded(child: AmountContainer()),
+                    Expanded(child: ExpensesList(expenses: _registeredExpense)),
                   ],
                 ),
-                addHeight(20),
-                ExpensesChart(),
-                addHeight(20),
-                Row(
-                  children: [
-                    Text('Last 10 transactions',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w800, fontSize: 12.sp)),
-                    Spacer(),
-                    Text('view all'.toUpperCase(),
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.black))
-                  ],
-                ),
-                addHeight(15),
-                SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      MyListTile(),
-                    ],
-                  ),
-                )
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
--- lib/src/components/amount_container.dart
+++ lib/src/components/amount_container.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AmountContainer extends StatelessWidget {
-  const AmountContainer({super.key});
+  const AmountContainer(
+      {super.key,
+      required this.heading,
+      required this.price,
+      required this.bgColor});
+
+  final String heading;
+  final String price;
+  final Color bgColor;
 
   Widget addHeight(double height) => SizedBox(height: height.h);
   Widget addWidth(double width) => SizedBox(width: width.w);
@@ -13,21 +21,24 @@ class AmountContainer extends StatelessWidget {
       height: 100.h,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20.r),
-          color: Colors.green.shade100),
+          color: bgColor),
       padding: EdgeInsets.symmetric(vertical: 20.h, horizontal: 12.w),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
-              Text('Earnings this month', style: TextStyle(fontSize: 10)),
+              Text(heading, style: TextStyle(fontSize: 10)),
               addWidth(0),
-              Icon(Icons.arrow_right)
+              Icon(
+                Icons.chevron_right,
+                size: 12,
+              )
             ],
           ),
           Spacer(),
           Text(
-            '#1,200.00',
+            '#${price}',
             style: TextStyle(fontWeight: FontWeight.w900, fontSize: 22.sp),
           )
         ],
