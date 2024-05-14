abff7a2051f2008700661a28d923f4daf0e98be2
Obasan Tolulope <obasantolu@gmail.com>
2024-05-14 11:53:39 +0100

    initial creation of themeProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:expense_tracker_app/src/components/persistent_nav.dart';
+import 'package:expense_tracker_app/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
-
+void main() => runApp(
+      ChangeNotifierProvider(
+          create: (context) => ThemeProvider(), child: const MyApp()),
+    );
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,9 +18,10 @@ class MyApp extends StatelessWidget {
       designSize: Size(375, 812),
       builder: (_, child) {
         return MaterialApp(
+          
           title: 'My Expense tracker',
           home: PersistentNav(),
-          theme: ThemeData(fontFamily: GoogleFonts.montserrat().fontFamily),
+          theme: (ThemeData(fontFamily: GoogleFonts.montserrat().fontFamily)),
         );
       },
     );
---
+++ lib/theme/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    background: Colors.white,
+
+  ),
+);
+
+ThemeData darkMode = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    background: Colors.grey.shade900,
+  ),
+);
